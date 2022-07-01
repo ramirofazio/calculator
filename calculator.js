56a379a169992b469ee4450a0ae4f05008d24289
Ramiro <ramifazio@gmail.com>
2022-07-01 17:45:34 -0300

    Logic on the js file

--- calculator.js
+++ calculator.js
@@ -1,4 +1,72 @@
 window.onload = function () {
-    let screen = document.getElementsByClassName("screenText")
-    console.log(screen[0].innerHTML)
-}
+    const screenText = document.getElementsByClassName("screenText");
+    const input = document.getElementsByClassName("input");
+    let leftNumbers;
+    let rightNumbers;
+    let operator;
+    let result;
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].addEventListener("click", function (e) {
+            console.log(typeof parseInt(e.target.value))
+            if (e.target.value === "=") {
+                rightNumbers = screenText[0].innerHTML.slice(leftNumbers.length + 1, screenText[0].innerHTML.length);
+                if (operator === "+") {
+                    console.log(leftNumbers, "", rightNumbers)
+                    result = parseInt(leftNumbers) + parseInt(rightNumbers);
+                    console.log(result)
+                }
+                if (operator === "-") {
+                    result = parseInt(leftNumbers) - parseInt(rightNumbers);
+                    console.log(result)
+                }
+                if (operator === "/") {
+                    result = parseInt(leftNumbers) / parseInt(rightNumbers);
+                    console.log(result)
+                }
+                if (operator === "*") {
+                    result = parseInt(leftNumbers) * parseInt(rightNumbers);
+                    console.log(result)
+                }
+                if (operator === "%") {
+                    result = parseInt(leftNumbers) % parseInt(rightNumbers);
+                    console.log(result)
+                }
+                screenText[0].innerHTML = result;
+            } else {
+
+                if (e.target.value === "+") {
+                    leftNumbers = screenText[0].innerHTML;
+                    operator = "+";
+                }
+                if (e.target.value === "-") {
+                    leftNumbers = screenText[0].innerHTML;
+                    operator = "-";
+                }
+                if (e.target.value === "/") {
+                    leftNumbers = screenText[0].innerHTML;
+                    operator = "/";
+                }
+                if (e.target.value === "*") {
+                    leftNumbers = screenText[0].innerHTML;
+                    operator = "*";
+                }
+                if (e.target.value === "%") {
+                    leftNumbers = screenText[0].innerHTML;
+                    operator = "%";
+                }
+                if (e.target.value === "Retr" || e.target.value === "C") {
+                    screenText[0].innerHTML = "0";
+                } else {
+                    if (screenText[0].innerHTML !== "0") {
+                        screenText[0].innerHTML += e.target.value;
+                    } else {
+                        screenText[0].innerHTML = e.target.value;
+                    }
+                }
+            }
+
+        }
+        );
+    };
+};
